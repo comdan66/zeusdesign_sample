d257ac1e7acf0b26c7cd02ef8e314797ec5795e6
OA Wu <comdan66@gmail.com>
2017-01-16 15:47:45 +0800

    Add sample.

---
+++ chrome_console/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+console.log ("Hello World, Console Log!");
+console.info ("Hello World, Console Info!");
+console.error ("Hello World, Console Error!");
